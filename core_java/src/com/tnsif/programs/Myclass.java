3e65839d07acd1928ba1d98ea5017bb0af0efb31
Krishna <Krishna@LAPTOP-P7KCLIUU>
2023-09-21 11:29:22 +0530

    first commit

---
+++ core_java/src/com/tnsif/programs/Myclass.java
@@ -0,0 +1,14 @@
+package com.tnsif.programs;
+
+public class Myclass {
+	public static void main(String[]args) {
+		int value=(int)(9/2.0d);
+		double value2=101d/6d;
+		double value3=101/61;
+		System.out.println("value="+value);
+		System.out.println("value2="+value2);
+		System.out.println("value3="+value3);
+		
+	}
+
+}
