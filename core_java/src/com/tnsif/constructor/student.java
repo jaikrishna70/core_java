8de122ee69aad1b88712fa1f8f9c820dfde6b1a9
Krishna <Krishna@LAPTOP-P7KCLIUU>
2023-09-28 20:44:52 +0530

    First commit

---
+++ core_java/src/com/tnsif/constructor/student.java
@@ -0,0 +1,16 @@
+package com.tnsif.constructor;
+
+public class student {
+	student()
+	{
+		System.out.println("Student object is created");
+		
+	}
+	public static void main (String[] args) {
+		student s=new student();
+		System.out.println(s);
+		
+		
+	}
+
+}
