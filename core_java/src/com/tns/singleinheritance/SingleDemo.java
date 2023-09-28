8ac3eddde5e39797b60cd365cd8f00ca5a220879
Krishna <Krishna@LAPTOP-P7KCLIUU>
2023-09-28 20:46:10 +0530

    First commit

---
+++ core_java/src/com/tns/singleinheritance/SingleDemo.java
@@ -0,0 +1,12 @@
+package com.tns.singleinheritance;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st=new Student("krishna",724739,"bhimavaram",79952);
+		System.out.println(st);
+
+	}
+
+}
